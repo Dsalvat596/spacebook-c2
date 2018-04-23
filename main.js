be73300207bd3ab64d1583ec0e51549c38b3f3f4
dsalvat596 <dsalvat596@gmail.com>
2018-04-23 13:18:32 +0300

    some tweaks, added comments to DOM

--- main.js
+++ main.js
@@ -1,66 +1,71 @@
 var posts = [];
 
-var userComments = []; //array of users and their comments
+
 
 const getTime = () => {
-    var d = new Date();
-    var n = d.getTime()
-    return n;
+  var d = new Date();
+  var n = d.getTime()
+  return n;
 }
 
 
 var createPost = function (string, num) {
-    var post = {
-        text: string,
-        id: num
-    };
-    posts.push(post);
+  var post = {
+    text: string,
+    id: num,
+    comments: []
+  };
+  posts.push(post);
 }
 
-// creates a username and comment pair, pushes to array
-var createUserCom = function (user, comm) {
-    var userCom = {
-        usr: user,
-        cmt: comm
-    }
-    userComments.push(userCom);
-}
+
 
 $('.add-post').click(function () {
-    var postInput = $('#post-name').val();
-    var postNum = getTime();
+  var postInput = $('#post-name').val();
+  var postNum = getTime();
 
-    createPost(postInput, postNum);
-    renderPosts();
+  createPost(postInput, postNum);
+  renderPosts();
 });
 
 // on click, saves username and comment to array
 $('.posts').on('click', '.comsubmit', function () {
-    var userNameTemp = $('.username').val();
-    var commTemp = $('.comment').val();
+  var clickedId = $(this).parent().data().id;
+  var userNameTemp = $('.username').val();
+  var commTemp = $('.comment').val();
 
-    createUserCom(userNameTemp, commTemp);
+  for (let i = 0; i < posts.length; i++) {
+    if (posts[i].id === clickedId) {
+      var comObj = {
+        user: userNameTemp,
+        comment: commTemp
+      };
+      posts[i].comments.push(comObj);
+    }
+  };
+ 
+    $('.post').append("<p>" + userNameTemp + ': ' + commTemp + "</p>")
 });
 
 var renderPosts = function () {
-    $('.posts').empty();
-    for (let i = 0; i < posts.length; i++) {
-        $('.posts').append("<p " + "class='post' " + "data-id =" + posts[i].id + ">" + posts[i].text + 
-        ' <button type="button" class="btn btn-danger remove">REMOVE</button>' + 
-        '<br> <input type="text" class="username" placeholder="username">'+
-        '<input type="text" class="comment" placeholder="begin comment">'+
-        '<button type="input" class="btn btn-success comsubmit">Submit</button></p>')
-    }
+  $('.posts').empty();
+  for (let i = 0; i < posts.length; i++) {
+    $('.posts').append("<p " + "class='post' " + "data-id =" + posts[i].id + ">" + posts[i].text +
+      ' <button type="button" class="btn btn-danger remove">REMOVE</button>' + 
+      '<br> <input type="text" class="username" placeholder="username">' +
+      '<input type="text" class="comment" placeholder="begin comment">' +
+      '<button type="input" class="btn btn-success comsubmit">Submit</button></p>')
+  }
 }
 
 // on click, selects the id# attribute, itereates and compares to find that obj in the array and splice it out
 $('.posts').on('click', '.remove', function () {
-    var clicked = $(this).parent().data().id;
-    for (j = 0; j < posts.length; j++) {
-        if (posts[j].id === clicked) {
-            posts.splice(j, 1);
-        }
+  var clicked = $(this).parent().data().id;
+  for (j = 0; j < posts.length; j++) {
+    if (posts[j].id === clicked) {
+      posts.splice(j, 1);
     }
-    renderPosts();
+  }
+  renderPosts();
 
 });
